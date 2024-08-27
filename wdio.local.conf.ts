89ea995696c734cc9125b4f1188e75e80b925334
Rahul Hovale <rahulhovale@gmail.com>
2024-08-27 13:00:09 +0100

    ci yaml config

--- wdio.local.conf.ts
+++ wdio.local.conf.ts
@@ -1,14 +1,12 @@
 import { config as sharedConfig } from "./wdio.shared.conf.ts";
+import {ReportGenerator, HtmlReporter} from 'wdio-html-nice-reporter';
 
 export const config: WebdriverIO.Config = {
   ...sharedConfig,
   ...{
     capabilities: [
       {
-        browserName: "chrome",
-        "wdio:devtoolsOptions": {
-          headless: false,
-        },
+        browserName: "chrome",        
       },
     ],
   },
